9fd546306b490473b566749c2f6cb96ef43b31d3
Ваше имя < ->
2023-01-22 20:06:09 +0300

    add PrintArray method

--- FinalWork/Program.cs
+++ FinalWork/Program.cs
@@ -42,3 +42,13 @@ string[] ArrangeArrayOfElementWithLongLessThatCertainNumber(string[] customersAr
     }
     return final;
 }
+
+// Печать массива
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
